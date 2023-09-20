a5cf7785d6cbf869c58c79bc8a2cc9c193f64742
Harsh Palan <harshpalan14@gmail.com>
2023-09-20 12:08:00 -0400

    Harsh's Gulab Jamun Recipe

---
+++ gulabjamun.js
@@ -0,0 +1,27 @@
+export const RECIPE = {
+    title: 'Gulab Jamun Recipe',
+    imgSrc: require('../assets/gulabjamun.jpg'),
+    description:
+      'It is a traditional Indian dessert, known as Gulab Jamun. It is a milk-solid-based sweet. It is soaked in a sugar syrup flavored with cardamom seeds and rosewater.',
+    ingredients: [
+      { name:'Milk powder', measure: '1 cup' },
+      { name:'All-purpose flour', measure: '1/4 cup' },
+      { name:'Ghee (clarified butter)', measure: '1/4 cup' },
+      { name:'Milk', measure: '1/4 cup' },
+      { name:'Baking soda', measure: '1/4 tsp' },
+      { name:'Cardamom powder', measure: 'A pinch' },
+      { name:'Oil for deep frying', measure: 'As required' },
+    ],
+    instructions: [
+        'In a mixing bowl, combine milk powder, all-purpose flour, ghee, milk, baking soda, and cardamom powder.',
+        'Knead the mixture into a soft dough.',
+        'Divide the dough into small portions and shape them into smooth balls.',
+        'Heat oil in a pan over medium heat.',
+        'Fry the dough balls until they turn golden brown.',
+        'Remove the fried balls and drain excess oil on a paper towel.',
+        'Prepare sugar syrup by dissolving 1 cup of sugar in 1 cup of water over medium heat.',
+        'Boil the syrup until it reaches a one-string consistency.',
+        'Drop the fried dough balls into the hot sugar syrup and let them soak for a few hours.',
+        'Gulab Jamuns are ready to serve once they absorb the syrup and become soft.',
+    ],
+  }
