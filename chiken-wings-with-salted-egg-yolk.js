568b8546d6ef564e6ac37fc113a46922b1f191b4
Liqian Zhang <lzhang2017@gmail.com>
2023-09-20 12:19:33 -0400

    Liqian's recipe

---
+++ chiken-wings-with-salted-egg-yolk.js
@@ -0,0 +1,30 @@
+export const RECIPE = {
+    title: 'Chicken Wings with Salted Egg Yoke',
+    imgSrc: require('../assets/wings.png'),
+    description:
+      "Chicken Wings with Salted Egg Yoke is a famous dish I've loved since I was very young. The combination of crispy fried chicken wings with the luxurious texture and unique taste of salted egg yolk creates a delightful dish that's both savory and slightly sweet. It is best to pair it with rice.",
+    ingredients: [
+      {measure:'1½ lbs', detail:'chicken wings'},
+      {measure:'1 tsp', detail:'salt'},
+      {measure:'1 tsp', detail:'garlic powder'},
+      {measure:'3/4 cup', detail:'All purpose flour'},
+      {measure:'¼ cup', detail:'corn starch'},
+      {measure:'1', detail:'large egg'},
+      {measure:'1 cup', detail:'buttermilk'},
+      {measure:'1 cup', detail:'coarse sweet potato starch'},
+      {measure:'5', detail:'salted egg yolks'},
+      {measure:'3 tbsp', detail:'butter'},
+      {measure:'4 cloves', detail:'garlic minced'},
+      {measure:'1 tsp', detail:'salt'},
+      {measure:'1 tsp', detail:'sugar'},
+
+    ],
+    instructions: [
+      'Pat dry and season your chicken wings with salt, garlic powder and onion powder.',
+      'Mix together the buttermilk and egg for your wet batter.',
+      'Drop chicken wings into corn starch, then into the wet batter, and coat well in coarse sweet potato starch.',
+      'Using a knife, smash your salted egg yolks and chop until a fine powder.',
+      'In a pan, heat up butter over medium heat and add garlic. stir 30 seconds.',
+      'Add chopped salted egg yolks and stir until it begins to melt. If it doesnt melt after 2 minutes, add more oil.',
+    ],
+  }
