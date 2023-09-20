99359da2419c93494f3cc6bbae3f0fe9550c4bbe
koeefu <102726954+koeefu@users.noreply.github.com>
2023-09-20 12:24:47 -0400

    Add files via upload

--- recipe-data.js
+++ recipe-data.js
@@ -1,18 +1,28 @@
 export const RECIPE = {
-    title: 'Kaya toast',
-    imgSrc: require('../assets/kaya.png'),
+    title: 'Stir-Fried Tomato and Scrambled Eggs',
+    imgSrc: require('../assets/dish.jpg'),
     description:
-        "This is one of my favorite Singaporean dishes to make. 'Kaya', which means coconut jam in Malay, is sandwiched between toasted bread with slices of cold butter. It's a staple breakfast dish in Singapore and Malaysia, typically served with soft-boiled eggs and coffee.",
+      "Stir-fried tomato and scrambled eggs are a common Chinese dish, and it was also the first dish I learned how to make. However, the point is I don't eat eggs, so I had never known the taste of the dish I cooked for my family : D",
     ingredients: [
-        {measure:'2 slices', name:'Bread'},
-        {measure:'2 slices', name:'Cold salted butter'},
-        {measure:'2 tbsp', name:'Kaya jam (Coconut butter)'},
+      {measure: '4', name: 'Large Tomato'},
+      {measure: '4', name: 'Egg'},
+      {measure: '2 Tbsp', name: 'Water'},
+      {measure: '1/4 Tbsp', name: 'Salt'},
+      {measure: '1/4 Cup', name: 'Oil'},
+      {measure: '3 Tbsp', name: 'Ketchup'}
+      // '4 Large Tomatos',
+      // '4 Eggs',
+      // '2 Tbsp Water',
+      // '1/4 Tbsp Salt',
+      // '1/4 Cup Oil',
+      // '3 tbsp Ketchup',
     ],
     instructions: [
-        'Cut off the crust of the bread.',
-        'Toast the bread to your liking.',
-        'Spread the kaya jam on both slices of the bread.',
-        'Place a few slices of thin cold butter on one of the toast.',
-        'Sandwich the toasts and cut in half.',
+      'Cut tomatoes into small wedges',
+      'Crack 4 eggs into a bowl and season with 1/4 teaspoon salt',
+      'Beat eggs for a while',
+      'Add 1/4 cup of oil and eggs to the wok and immediately scramble the eggs',
+      'Add the tomatoes, water and ketchup',
+      'Mix everything together and stir-fry for 1-2 minutes, until the tomatoes are completely softened',
     ],
-}
+  }
